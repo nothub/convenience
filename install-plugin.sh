9dd522f6dfc35e2fb0dc819d60fbba4f263b07ac
blockparole <48992448+blockparole@users.noreply.github.com>
2020-08-22 19:30:29 +0200

    init

---
+++ install-plugin.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "please provide artifact id as script argument!"
+  exit 1
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [ ! -d ../server/plugins ]; then
+  bash ./install-server.sh
+fi
+
+rm -f ../server/plugins/"$1"-*.jar
+cp ../target/"$1"-*.jar ../server/plugins
+
+echo "plugin installation complete"
