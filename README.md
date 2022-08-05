0692ba8af8793da8e1d057e694d86a229209ba09
hub <code@hub.lol>
2022-08-05 02:23:26 +0200

    bash rewrite

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
-Quick and easy local Minecraft server deployment.
+Download (and provision) Minecraft servers from the papermc.io api.
 
 ---
 
 ##### Usage
 
 ```
-Usage: paperdl.sh [-d=<path>] [-p=<port>] [-i=<path>...] [-v] [-h] [--] [version] [build]
-Download (and provision) Minecraft servers from the papermc.io api.
+Usage: paperdl [-d=<path>] [-p=<port>] [-a=<path>] [-i=<path>...] [-v] [-h|-?] [--] [version] [build]
+
+By default, the latest possible server release will be used.
+
 Options:
-  -d    <path>    Server directory  (default: ./server)
-  -p    <port>    Server port       (default: 25565)
-  -i    <path>    Plugin jar to install
-  -v              Enable verbose output
-  -h              Print this help and exit
+  -d <path>    Server directory       [default: server]
+  -p <port>    Server port            [default: 25565]
+  -a <path>    Server alias           [default: {{server}}/server.jar]
+  -i <path>    Plugin jar to install  (multiple possible)
+  -v           Enable verbose output
+  -h, -?       Print help and exit
+
+Examples:
+  paperdl 1.12.2 1337
+  paperdl -v 1.15
+  paperdl -d srv
+  paperdl -d /opt/srv
+  paperdl -p 9001
+  paperdl -a foo.jar
+  paperdl -a "/home/user/bar.jar"
+  paperdl -i foo.jar -i rel/bar.jar -i /abs/test.jar
 ```
 
 ---
--- paperdl.sh
+++ paperdl.sh
@@ -14,14 +14,29 @@ log() {
 usage() {
     set +o xtrace
     script_name="$(basename "${BASH_SOURCE[0]}")"
-    log "Usage: ${script_name} [-d=<path>] [-p=<port>] [-i=<path>...] [-v] [-h] [--] [version] [build]
-Download (and provision) Minecraft servers from the papermc.io api.
+    log "Download (and provision) Minecraft servers from the papermc.io api.
+
+Usage: ${script_name} [-d=<path>] [-a=<path>] [-p=<port>] [-i=<path>...] [-v] [-h|-?] [--] [version] [build]
+
+By default, the latest possible server release will be used.
+
 Options:
-  -d    <path>    Server directory  (default: ./server)
-  -p    <port>    Server port       (default: 25565)
-  -i    <path>    Plugin jar to install
-  -v              Enable verbose output
-  -h              Print this help and exit"
+  -d <path>    Server directory       [default: server]
+  -a <path>    Server alias           [default: {{server}}/server.jar]
+  -p <port>    Server port            [default: 25565]
+  -i <path>    Plugin jar to install  (multiple possible)
+  -v           Enable verbose output
+  -h, -?       Print help and exit
+
+Examples:
+  ${script_name} 1.12.2 1337
+  ${script_name} -v 1.15
+  ${script_name} -d srv
+  ${script_name} -d /opt/srv
+  ${script_name} -a foo.jar
+  ${script_name} -a \"/home/user/bar.jar\"
+  ${script_name} -p 9001
+  ${script_name} -i foo.jar -i rel/bar.jar -i /abs/test.jar"
 }
 
 check_dependency() {
@@ -35,18 +50,21 @@ unquote() {
     echo "${*//\"/""}"
 }
 
-server_dir=$(realpath "./server")
-port=25565
 plugins=()
-while getopts d:i:vh? opt; do
-    case $opt in
-    d) server_dir="$OPTARG" ;;
-    p) port="$OPTARG" ;;
-    i) plugins+=("$OPTARG") ;;
+while getopts d:a:p:i:vh? opt; do
+    case ${opt} in
+    d) server_dir="${OPTARG}" ;;
+    a) server_alias="${OPTARG}" ;;
+    p) server_port="${OPTARG}" ;;
+    i) plugins+=("${OPTARG}") ;;
     v) set -o xtrace ;;
-    h | \? | *)
+    h | \?)
         usage
         exit 0
+        ;;
+    *)
+        usage
+        exit 1
         ;;
     esac
 done
@@ -55,12 +73,25 @@ shift $((OPTIND - 1))
 check_dependency curl
 check_dependency jq
 
+# default options
+set +o nounset
+if [[ -z ${server_dir} ]]; then server_dir="server"; fi
+if [[ -z ${server_port} ]]; then server_port=25565; fi
+if [[ -z ${server_alias} ]]; then server_alias="${server_dir}/server.jar"; fi
+set -o nounset
+
+# resolve absolute pathes
+server_dir=$(realpath --no-symlinks --canonicalize-missing "${server_dir}")
+server_alias=$(realpath --no-symlinks --canonicalize-missing "${server_alias}")
+
+# read args
 set +o nounset
 version=$1
 build=$2
 shift 2 || true
 set -o nounset
 
+# fetch latest version
 if [[ -z ${version} ]]; then
     versions_raw=$(curl --silent --location -H "Accept: application/json" "${url_base}" | jq -c '.versions[]')
     mapfile -t versions < <(unquote "${versions_raw}" | grep --perl-regexp "^\d+\.\d+(\.\d)?$" | sort --version-sort --reverse)
@@ -68,6 +99,7 @@ if [[ -z ${version} ]]; then
     version="${versions[0]}"
 fi
 
+# fetch latest build
 if [[ -z ${build} ]]; then
     builds_raw=$(curl --silent --location -H "Accept: application/json" "${url_base}/versions/${version}" | jq -c '.builds[]')
     mapfile -t builds < <(echo "${builds_raw}" | sort --numeric-sort --reverse)
@@ -78,24 +110,24 @@ fi
 download_raw=$(curl --silent --location -H "Accept: application/json" "${url_base}/versions/${version}/builds/${build}" | jq -c '.downloads.application')
 download_file="$(unquote "$(echo "${download_raw}" | jq -c '.name')")"
 download_hash="$(unquote "$(echo "${download_raw}" | jq -c '.sha256')")"
-log "download file:  ${download_file}"
-url_download="${url_base}/versions/${version}/builds/${build}/downloads/${download_file}"
-log "download url:   ${url_download}"
+download_url="${url_base}/versions/${version}/builds/${build}/downloads/${download_file}"
+log "download url:   ${download_url}"
 
 log "server dir:     ${server_dir}"
 mkdir -p "${server_dir}"
-(cd "${server_dir}" && curl --location --progress-bar --remote-name --remote-time "${url_download}")
+(cd "${server_dir}" && curl --location --progress-bar --remote-name --remote-time "${download_url}")
 echo "${download_hash}  ${server_dir}/${download_file}" | sha256sum -c -
 
 { # default config
-    echo "motd='${version} test server'"
-    echo "server-port=25565"
+    echo "motd='${version} server'"
+    echo "server-port=${server_port}"
     echo "spawn-protection=0"
 } >"${server_dir}/server.properties"
 
 # create alias
-if [[ -L ${server_dir}/server.jar ]]; then rm -f "${server_dir}/server.jar"; fi
-ln -s "${server_dir}/${download_file}" "${server_dir}/server.jar"
+log "server alias:   ${server_alias}"
+if [[ -L ${server_alias} ]]; then rm -f "${server_alias}"; fi
+ln -s "${server_dir}/${download_file}" "${server_alias}"
 
 # disable metrics
 bstats_config="${server_dir}/plugins/bStats/config.yml"
@@ -104,11 +136,10 @@ echo "enabled: false" >"${bstats_config}"
 
 # add plugins
 for plugin in "${plugins[@]}"; do
-    log "installing plugin: ${plugin}"
-    cp "${plugin}" "${server_dir}/plugins/"
+    cp --verbose "${plugin}" "${server_dir}/plugins/" || true
 done
 
-#    # eula
+# eula
 #    eula_file: Path = args.server_dir.joinpath('eula.txt')
 #    eula_accepted = True if os.getenv('MC_EULA') is not None else False
 #    if not eula_accepted and eula_file.exists() and eula_file.is_file():
